8416a7ab45fcc28fcaf6744f9684541000595e97
igordon215 <igordon215@gmail.com>
2024-07-02 17:08:24 -0400

    lad fin

--- Step3/ReadThree.java
+++ Step3/ReadThree.java
@@ -16,17 +16,17 @@ public class ReadThree
             // Write text just like we would to the console
             fileOut.println("Hello ");
             // Clode out file
-            fileOut.close();  
+            fileOut.close();
 
             System.out.println("Done!");
-            
+
             // Read in file to verify it
             Scanner fileIn = new Scanner(new File("output.txt"));
-            
+
             System.out.println("Reading from file ... ");
-            while (fileIn.hasNext()) 
+            while (fileIn.hasNext())
             {
-                String lineIn = fileIn.nextLine(); 
+                String lineIn = fileIn.nextLine();
                 System.out.println(lineIn);
             }
             System.out.println("Done!");
--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,18 +1,46 @@
 package Step4;
 
+import java.io.FileNotFoundException;
+import java.io.OptionalDataException;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
-public class ReadFour
-{
+public class ReadFour {
     /**
-     * Yes, you can look at the other 
+     * Yes, you can look at the other
      * examples in this lab to build your solution here.
      */
-        public static void main(String[] args)
-        {
-            // Print out a running total of all the
-            // values in the input file.
+    public static void main(String[] args) throws FileNotFoundException {
+        // Print out a running total of all the
+        // values in the input file.
+
+        try {
+            Scanner fileIn = new Scanner(new File("Step4/input.txt"));
+            int sum = 0;
+
+            while (fileIn.hasNext())
+            {
+                // Reads the entire line as a string
+                String lineIn = fileIn.nextLine();
+                // Split the line into a String array
+                String[] input = lineIn.split(",");
+
+                for (int i = 0; i < input.length; i ++) {
+                    sum += Integer.parseInt(input[i]);
+                }
+                System.out.println("Running total = " + sum);
+            }
         }
+        catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
 }
+
+
+//        {
+//            // Print out a running total of all the
+//            // values in the input file.
+//        }
+//}
